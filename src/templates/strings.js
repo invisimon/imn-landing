f60779487e40cc069afe91839c255be6ec8f233a
INVISIMON <109383207+invisimon@users.noreply.github.com>
2023-03-14 21:00:36 -0500

    Update strings.js

--- src/templates/strings.js
+++ src/templates/strings.js
@@ -8,7 +8,7 @@ export default function TXT(entry) {
 
 const records = {
   NOTICE: [
-"Invisimon is a media franchise project that, briefly speaking, mixes features of Pokemon and Stranger Things. Our shared universe consists of two parallel Earths, linked by an unusual black hole and watched by 8 Ethereal higher life forms. It's a story about the Subjective Realm - where inhabit our emotions, ideas and spirits - exploring the effects of the invisible things in our lives. #NeverAlone #WhatDoYouSee?",
-"Invisimon is a media franchise project that, briefly speaking, mixes features of Pokemon and Stranger Things. Our shared universe consists of two parallel Earths, linked by an unusual black hole and watched by 8 Ethereal higher life forms. It's a story about the Subjective Realm - where inhabit our emotions, ideas and spirits - exploring the effects of the invisible things in our lives. #NeverAlone #WhatDoYouSee?",
+"Em breve",
+"Soon",
   ]
 };
